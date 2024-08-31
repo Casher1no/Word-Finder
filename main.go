7eace7ee27930a7fc4b57888436c550bfe54650a
Maikls <maikls.zalkalns@gmail.com>
2024-08-31 18:17:12 +0300

    Input and possible word response

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 	"strings"
 )
 
+const (
+	errWrongInput = "An error occured while reading input. Please try again."
+)
+
 func main() {
+	var err error
+	var reader *bufio.Reader
+
 	file, err := os.Open("words.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -27,17 +34,61 @@ func main() {
 		fmt.Println("Error reading file:", err)
 	}
 
-	// fmt.Println(words)
+	minLength := 0
+	maxLength := 0
+	givenLetters := ""
 
-	fmt.Print("Enter letters: ")
-	reader := bufio.NewReader(os.Stdin)
-
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("An error occured while reading input. Please try again", err)
+	fmt.Print("Minimal word length: ")
+	if _, err = fmt.Scanf("%d\n", &minLength); err != nil {
+		fmt.Println(errWrongInput, err)
 		return
 	}
 
-	input = strings.TrimSuffix(input, "\n")
-	fmt.Println(input)
+	fmt.Print("Maximal word length: ")
+	if _, err = fmt.Scanf("%d\n", &maxLength); err != nil {
+		fmt.Println(errWrongInput, err)
+		return
+	}
+
+	fmt.Print("Given letters: ")
+	reader = bufio.NewReader(os.Stdin)
+
+	givenLetters, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(errWrongInput, err)
+		return
+	}
+
+	givenLetters = strings.TrimSpace(givenLetters)
+	givenLetters = strings.ToLower(givenLetters)
+
+	var possibleWords []string
+	for _, word := range words {
+		word = strings.ToLower(word)
+		wordLength := len(word)
+		if wordLength >= minLength && wordLength <= maxLength && constructWord(word, givenLetters) {
+			possibleWords = append(possibleWords, word)
+		}
+	}
+
+	fmt.Printf("Possible words (%d):\n", len(possibleWords))
+	for _, word := range possibleWords {
+		fmt.Println(word)
+	}
+}
+
+func constructWord(word string, givenLetters string) bool {
+	letterCount := make(map[rune]int)
+
+	for _, letter := range givenLetters {
+		letterCount[letter]++
+	}
+
+	for _, letter := range word {
+		if letterCount[letter] == 0 {
+			return false
+		}
+		letterCount[letter]--
+	}
+	return true
 }
